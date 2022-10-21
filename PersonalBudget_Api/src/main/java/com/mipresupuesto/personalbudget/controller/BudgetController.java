b449a2c59d929dfef1954705b3e270678ca329a9
AlanPineda0430 <alanpc3010@gmail.com>
2022-10-20 21:28:07 -0500

    controller

--- PersonalBudget_Api/src/main/java/com/mipresupuesto/personalbudget/controller/BudgetController.java
+++ PersonalBudget_Api/src/main/java/com/mipresupuesto/personalbudget/controller/BudgetController.java
@@ -1,15 +1,30 @@
 package com.mipresupuesto.personalbudget.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mipresupuesto.personalbudget.application.command.interfaces.CreateBudgetPort;
 
 @RestController
 @RequestMapping("api/v1/budget")
 public class BudgetController {
 	
+	@Autowired
+	private CreateBudgetPort createBudgetPort;
+	
+	
 	@PostMapping
 	public BudgetDTO Create(@RequestBody BudgetDTO budget) {
+		createBudgetPort.execute(budget);
 		return budget;
 	}  
 
+	@GetMapping
+	public String saludar() {
+		return "hola mundo";
+	}
 }
--- PersonalBudget_Api/src/main/java/com/mipresupuesto/personalbudget/init/PersonalBudgetApiApplication.java
+++ PersonalBudget_Api/src/main/java/com/mipresupuesto/personalbudget/init/PersonalBudgetApiApplication.java
@@ -2,8 +2,10 @@ package com.mipresupuesto.personalbudget.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= { "com.mipresupuesto.personal")
 public class PersonalBudgetApiApplication {
 
 	public static void main(String[] args) {
